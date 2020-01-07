cb5f4e97870a0600dd74384ca0306796b5300e32
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2020-01-08 00:06:25 +0100

    sm6150-common: Build ParanoidDoze
    Change-Id: I15aa87442124d30d6f69eb8d2c10ce47d26475d5

--- sm6150.mk
+++ sm6150.mk
@@ -128,10 +128,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml
 
-# Doze
-PRODUCT_PACKAGES += \
-    XiaomiDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3-service.clearkey
@@ -250,6 +246,10 @@ PRODUCT_ENFORCE_RRO_TARGETS += *
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
+
+# ParanoidDoze
+PRODUCT_PACKAGES += \
+    ParanoidDoze
 
 # Perf
 PRODUCT_PACKAGES += \
