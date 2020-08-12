a97578b2f22e5a342a62f9b47153a9955f8b5a9b
Lucchetto <lucchetto.tie@live.com>
2020-08-12 14:14:36 +0200

    sm6150-common: Create dummy libqti-perfd-client
    * proprietary perfd blobs can finally be nuked without breaking goodix
    * we could even map the functions to use libperfmgr powerhints
    
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb

--- sm6150.mk
+++ sm6150.mk
@@ -252,6 +252,10 @@ PRODUCT_ENFORCE_RRO_TARGETS += *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
 
+# Perf
+PRODUCT_PACKAGES += \
+    libqti-perfd-client
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
