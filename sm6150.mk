4b06118b0566c2d5bec9d8cd2b86794205910ba5
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2021-04-26 09:13:17 +0530

    sm6150-common: Decommonize QCOM post boot script
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>

--- sm6150.mk
+++ sm6150.mk
@@ -200,7 +200,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/bin/init.qcom.early_boot.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.early_boot.sh \
-    $(LOCAL_PATH)/rootdir/bin/init.qcom.post_boot.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.post_boot.sh \
     $(LOCAL_PATH)/rootdir/bin/init.qcom.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.sh
 
 # IRSC
